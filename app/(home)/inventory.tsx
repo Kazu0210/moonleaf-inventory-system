da60db4e9f2cf2e915a43ee3a9e0f02ce955ca31
Kazu0210 <clffrdkyl@gmail.com>
2025-02-15 00:40:29 +0800

    added search bar in inventory page

--- app/(home)/inventory.tsx
+++ app/(home)/inventory.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, TextInput } from "react-native";
 import { Link } from 'expo-router';
 import { SQLiteProvider, useSQLiteContext } from 'expo-sqlite';
 import { Image, ScrollView } from 'react-native';
@@ -9,8 +9,29 @@ import styles from '../../assets/styles/home'
 export default function Inventory() {
     return (
         <ScrollView>
-            <View style={styles.container}>
+            <View style={{ flex: 1, flexDirection: 'column' }}>
                 <Text>Inventory Screen</Text>
+                <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', backgroundColor: 'blue', padding: 5 }}>
+                    <TextInput placeholder="Search here" style={{ backgroundColor: '#fff', width: 250, height: 30, borderRadius: 5, fontSize: 12, padding: 0, margin: 0, textAlignVertical: 'center' }} />
+                    {/* remove margin or padding on the text input */}
+                    <TouchableOpacity style={{
+                        backgroundColor: 'red',
+                        width: 70,
+                        maxWidth: 70,
+                        height: 30,
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: 'center',
+                        borderRadius: 5,
+                        margin: 5
+                    }}>
+                        <Text style={{
+                            color: '#fff',
+                            fontSize: 12,
+                            fontWeight: 'bold'
+                        }} >Search</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </ScrollView>
     );
